dc7691201fab0afc3d9fe0d1f5383412dcce5bf5
tanay-18 <tanaygoyal2004@gmail.com>
2025-03-12 22:40:36 +0530

    Add files via upload

---
+++ snake_game/scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score=0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"score:{self.score}",align="center",font=("courier",24,"normal"))
+        self.hideturtle()
+    def increase_score(self):
+        self.score+=1
+        self.clear()
+        self.write(f"score:{self.score}",align="center",font=("courier",24,"normal"))
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER",align="center")
